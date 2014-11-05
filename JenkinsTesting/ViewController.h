406d8da9a794274918200b9c3c472363c98a690f
sandbox340 <sandbox340@gmail.com>
2014-11-05 16:39:17 -0700

    Initial Commit

---
+++ JenkinsTesting/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JenkinsTesting
+//
+//  Created by Sand Box on 11/5/14.
+//  Copyright (c) 2014 Sand Box. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ JenkinsTesting/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  JenkinsTesting
+//
+//  Created by Sand Box on 11/5/14.
+//  Copyright (c) 2014 Sand Box. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
